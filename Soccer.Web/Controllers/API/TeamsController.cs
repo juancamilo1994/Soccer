b6185c72a8359232e372ab02f9d5e65d566ca3a5
juan martinez <juankmilo0014@hotmail.com>
2020-07-20 00:42:13 -0500

    Api tournaments and json

---
+++ Soccer.Web/Controllers/API/TeamsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Soccer.Web.Data;
+using Soccer.Web.Data.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Soccer.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public TeamsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Teams
+        [HttpGet]
+        public IEnumerable<TeamEntity> GetTeams()
+        {
+            return _context.Teams.OrderBy(t => t.Name);
+        }
+
+        // GET: api/Teams/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTeamEntity([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            TeamEntity teamEntity = await _context.Teams.FindAsync(id);
+
+            if (teamEntity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(teamEntity);
+        }
+
+       
+    }
+}
